117fb6f86251c013b3eed0bf247c2083d78c0389
Joel Wilson <joelwilson2000@gmail.com>
2026-07-24 19:05:39 +0530

    Customer_Generator_1 & Logger

---
+++ utils/logger.py
@@ -0,0 +1,14 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+console_handler = logging.StreamHandler()
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+console_handler.setFormatter(formatter)
+
+if not logger.handlers:
+    logger.addHandler(console_handler)
+
+logger.propagate = False
